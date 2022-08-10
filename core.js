f8183279adf19fe330f57a71f964d76cc6285010
kramarukha <vladislav.kramarukha@magenta-technology.com>
2022-08-10 11:57:47 +0400

    (feature): Created core class Types and add types in class fields

---
+++ core.js
@@ -0,0 +1,30 @@
+export class Types {
+    static String = 'string'
+    static Number = 'number'
+    static Int = 'int'
+    static Boolean = 'boolean'
+    static BigInt = 'bigint'
+    static Float = 'float'
+    static Object = 'object'
+    static Symbol = 'symbol'
+    static Func = 'function'
+    static Undefined = 'undefined'
+    static NaN = 'NaN'
+
+    static Array = 'array'
+    static EmptyArray = 'emptyArray'
+    static ArrayOfString = 'arrayOfString'
+    static ArrayOfNumber = 'arrayOfNumber'
+    static ArrayOfFloat = 'arrayOfFloat'
+    static ArrayOfInt = 'arrayOfInt'
+    static ArrayOfBigInt = 'arrayOfBigInt'
+    static ArrayOfBoolean = 'arrayOfBoolean'
+    static ArrayOfSymbol = 'arrayOfSymbol'
+    static ArrayOfFunc = 'arrayOfFunc'
+    static ArrayOfObject = 'arrayOfObject'
+    static ArrayOfArray = 'arrayOfArray'
+
+    static new = (type, value) => {
+
+    }
+}
